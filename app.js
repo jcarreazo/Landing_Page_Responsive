d5b726cf065056b0f64f1322d0b790194c7fb6dd
jcarreazo <jcarreazos.dev@gmail.com>
2021-06-28 16:51:22 -0500

    Finalizado de vista laptop

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-document.body.classList.add('js-loading');
-
-window.addEventListener("load", showPage, false);
-
-function showPage() {
-  document.body.classList.remove('js-loading');
-}
+$("div").addClass('js-loading');
+$(document).ready(()=>{
+  $('div').removeClass('js-loading');
+})
