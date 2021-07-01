1a5c7753f61964402c6bcfe84cfe40fbcf4e394c
jcarreazo <jcarreazos.dev@gmail.com>
2021-07-01 18:19:44 -0500

    Finalizado de menu y boton mobile

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const menu = $(".menu-bar");
 const nav = $(".menu-tablet-titles");
+const links=$(".menu-titles-container");
+$(".menu-container").hide();
 var Switch=true;
 const navSlide = () => {
+nav.css({"display":"none"})
+links.css({"display":"none"})
   menu.click(() => {
-    runEffect();
+    nav.css({"display":"flex"});
+    links.css({"display":"flex"});
+    setTimeout(() => {
+      runEffect();
+    }, 50);
     if(Switch==true){
         nav.css({"opacity": 1});
-      rotateClick();
+      setTimeout(() => {
+        rotateClick();
+      }, 100);
       $('html, body').css({
         overflow: 'hidden',
         height: '100%'
@@ -21,8 +31,12 @@ const navSlide = () => {
     });
     setTimeout(() => {
       nav.css({"opacity": 0})
-    }, 200);
+    }, 200); setTimeout(() => {
+      nav.css({"display":"none"});
+      links.css({"display":"none"});
+    }, 300);
     }
+    
     Switch=!Switch
   });
   
@@ -37,14 +51,26 @@ const rotateClick=()=>{
   $("#id-menu").css({"transform": "rotate(180deg)","transition":" 0.3s ease-in"})
   setTimeout(() => {
     $("#id-menu").attr("name", "x");
+    $("#id-menu").attr("color", "black");
   }, 200);
+  $(".menu-container").each( function() {
+    $(this).show({effect:"fade",duration:500})
+  });
+
   
 }
 const rotateBack=()=>{
   $("#id-menu").css({"transform": "rotate(0deg)","transition":" 0.3s ease-in"})
   setTimeout(() => {
   $("#id-menu").attr("name", "menu");
+  $("#id-menu").attr("color", "white");
   }, 200);
+  let increment=40
+  $(".menu-container").each( function() {
+    $(this).hide({effect:"fade",duration:250})
+    console.log(increment);
+  });
+
 }
 navSlide();
 
