5d2f9d368331c18dbba0977771f956d2ad9d4a30
jcarreazo <jcarreazos.dev@gmail.com>
2021-06-30 22:52:36 -0500

    Inicio diseño navbar

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-$("div").addClass('js-loading');
-$(document).ready(()=>{
-  $('div').removeClass('js-loading');
-})
+const navSlide = () => {
+  const menu = $(".menu-bar");
+  const nav = $(".menu-tablet-titles");
+
+  menu.click(() => {
+    let width=nav.css("width");
+    if(nav.css("margin-right")==width){
+      nav.animate({"margin-right":`-=${width}`});
+    }else{
+      nav.animate({"margin-right":`+=${width}`})
+    }
+  });
+  
+}
+navSlide();
